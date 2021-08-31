77522714981d49300a2162043825306fd10b5f08
Enrique Rodriguez <enrique.rodriguez@icloud.com>
2021-08-31 09:16:56 +0200

    Initial commit

---
+++ password-generator.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+password = []
+for i in range(4):
+    password.append(''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(5)))
+
+print '-'.join(password)
