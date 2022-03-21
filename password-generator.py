a5aa8a537a5c61ec01031f4b86b19009d7f55f2a
Enrique Rodriguez <enrique.rodriguez@icloud.com>
2022-03-21 17:51:39 +0100

    Python 3 compatibility

--- password-generator.py
+++ password-generator.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import random
 import string
 
@@ -5,4 +7,4 @@ password = []
 for i in range(4):
     password.append(''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(5)))
 
-print '-'.join(password)
+print ('-'.join(password))
